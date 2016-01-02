8f1da4d8a182d9b0bea85cf82c4dbb23e9da558e
daniele <daniele.email0@gmail.com>
2016-01-02 22:17:48 +0100

    Rename lcd_hello and update homepage

--- SDBuilder/target/var/www/index.php
+++ SDBuilder/target/var/www/index.php
@@ -7,6 +7,8 @@ echo "<center><h1>Welcome to $hostname</h1>";
 echo "$uname<br>";
 echo "<table border='1'><th>PIN</th><th>Usage</th><th>Dts</th>";
 echo "<tr><td>PA01</td><td>1-Wire</td><td>Y</td></tr>";
+echo "<tr><td>PA02</td><td>LCD Reset</td><td>Y</td></tr>";
+echo "<tr><td>PA03</td><td>LCD Backlight/td><td>Y</td></tr>";
 echo "</table></center>";
 
 ?>
