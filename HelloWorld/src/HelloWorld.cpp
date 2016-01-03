3a663a15c0b28b6982ce680e2182620b89553032
daniele <daniele.email0@gmail.com>
2016-01-03 07:59:23 +0100

    HelloWorld

---
+++ HelloWorld/src/HelloWorld.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Daniele
+// Version     :
+// Copyright   : GPL2
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	for(int i=0;i<10;i++) {
+		cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	}
+	return 0;
+}
