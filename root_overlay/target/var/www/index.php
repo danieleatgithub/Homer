70ca45dcb5cf7df0156c923439bf3831a8c80b42
daniele <daniele.email0@gmail.com>
2017-04-18 21:29:23 +0200

    Minors

--- root_overlay/target/var/www/index.php
+++ root_overlay/target/var/www/index.php
@@ -2,23 +2,31 @@
 <?php
 $hostname = gethostname ();
 $uname = php_uname ();
+$chrony_tracking=shell_exec("chronyc tracking"); 
 
 echo "<center><h1>Welcome to $hostname</h1>";
 echo "$uname<br>";
-echo "<table border='1'><th>PIN</th><th>Usage</th><th>Dts</th>";
-echo "<tr><td>PA01</td><td>1-Wire</td><td>Y</td></tr>";
-echo "<tr><td>PA02</td><td>LCD Reset</td><td>Y</td></tr>";
-echo "<tr><td>PA03</td><td>LCD Backlight/td><td>Y</td></tr>";
-echo "</table></center>";
+echo "<big>";
 
 echo "<hr>";
-echo "<center><big>";
+echo "<table  border='1'>";
+echo "<td><pre>".$chrony_tracking."</pre>";
+echo "</table>";
+
+echo "<hr>LINK";
 echo "<table  border='1'>";
 echo "<td><a href=data/>DATA FS</a>";
-echo "<td><a href=data/logs/homerd.txt>homerd.txt</a>";
-echo "<td><a href=data/logs/hdevice.txt>hdevice.txt</a>";
-echo "";
-echo "</table></center>";
+echo "<td><a href=data/log/homerd/homerd.txt>homerd.txt</a>";
+echo "<td><a href=data/log/homerd/hdevice.txt>hdevice.txt</a>";
+echo "<td><a href=data/log/homerd/sensors.txt>sensors.txt</a>";
+echo "</table>";
 
-
+echo "<hr>NEXT TODO";
+echo "<table  border='1'>";
+echo "<td>HW<td>RTC Battery<tr>";
+echo "<td>HW FIX<td>Display light flickering<tr>";
+echo "<td>SW<td>Sensor update log rolling month (log4cplus /etc/default/homerd.properties)<tr>";
+echo "<td>SW FIX<td>Reset MS on EV_KEY Key=106 (BUTTON_RIGHT) PRESSED ms=100 isLong=N<tr>";
+echo "</table>";
+echo "</center>";
 ?>
